b0c39968885c16f16a0bdd54092d220c075b911c
rkml14 <119961747+rkml14@users.noreply.github.com>
2023-01-24 21:45:08 -0500

    worked on index.html & script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,35 +4,41 @@
 
 //currentQuestion represents the whole question object (question to answer)
 let quizArray = [
-{question: "What is the common naming convention for variables in Javascript?",
-options:["Uppercase", "Hyphenate", "None", "Camelcase"],
-answer: 3,
-}, 
-{question: "Which of these is NOT a type of Javascript operator?",
-options:["Arthmetic", "Conditional", "Telephone", "Logical"],
-answer:2,
-},
-{question: "In HTML, the Javascript code is inserted between which element tag? ",
-options:["<head> </head>", "<script> </script>", "<h1> </h1>", "<div> </div>"],
-answer:1,
-},
-{question: "Arrays are used in Javascript to store:",
-options:["Booleans", "Numbers & Strings", "Other Arrays", "All of the Above"],
-answer:3,
-},
-{question: "What is a great tool that can be used during development & debugging? ",
-options:["console.log", "loops", "gitHub", "prayer"],
-answer:0,
-},
+  {
+    question: "What is the common naming convention for variables in Javascript?",
+    options: ["Uppercase", "Hyphenate", "None", "Camelcase"],
+    answer: 3,
+  },
+  {
+    question: "Which of these is NOT a type of Javascript operator?",
+    options: ["Arthmetic", "Conditional", "Telephone", "Logical"],
+    answer: 2,
+  },
+  {
+    question: "In HTML, the Javascript code is inserted between which element tag? ",
+    options: ["<head> </head>", "<script> </script>", "<h1> </h1>", "<div> </div>"],
+    answer: 1,
+  },
+  {
+    question: "Arrays are used in Javascript to store:",
+    options: ["Booleans", "Numbers & Strings", "Other Arrays", "All of the Above"],
+    answer: 3,
+  },
+  {
+    question: "What is a great tool that can be used during development & debugging? ",
+    options: ["console.log", "loops", "gitHub", "prayer"],
+    answer: 0,
+  },
 ];
 
 //Global variables for the timer   
 let currentTime = document.querySelector(".current-time");
 let timer = document.querySelector(".start-button");
 let highScore = document.querySelector("#reset-button");
+let submit = document.querySelector("#submit-button");
 let timerCount = 61;
 let timerPenalty = 10;
-let holdInterval = 0;  
+let holdInterval = 0;
 
 
 //Global variables for the questions
@@ -42,45 +48,45 @@ let introduction = document.querySelector("#introduction");
 let questionIndex = 0;
 
 //Global variables for added text
-let questionEl = document.createElement("div");
-let scoreEl = document.createElement("div");
+let questionEl = document.createElement('div');
+let scoreEl = document.createElement('p');
 
 //Global variables 
-let score = 0;  
+let score = 0;
 
 
 
 //Function to countdown timer and display it on the browswer
-  function startTimer() { 
-    introduction.classList.add("hide");
-    questionArea.classList.remove("hide"); 
-    timer.classList.add("hide");
-    renderQuestions();
-      holdInterval = setInterval(function () {
-          timerCount--;
-          currentTime.textContent = "Time left: " + timerCount;
+function startTimer() {
+  introduction.classList.add("hide");
+  questionArea.classList.remove("hide");
+  timer.classList.add("hide");
+  renderQuestions();
+  holdInterval = setInterval(function () {
+    timerCount--;
+    currentTime.textContent = "Time left: " + timerCount;
 
-          if (timerCount <= 0) {
-              clearInterval(holdInterval);
-              currentTime.textContent = "Out of Time";
-          }
-      }, 1000);
+    if (timerCount <= 0) {
+      clearInterval(holdInterval);
+      currentTime.textContent = "Out of Time";
     }
+  }, 1000);
+}
 
 //When start button clicked, calls startTimer function    
-  timer.addEventListener('click', startTimer);  
+timer.addEventListener('click', startTimer);
 
 
 function checkAnswer(index) {
   let currentQuestion = quizArray[questionIndex];
- 
+
   if (currentQuestion.answer === index) {
     score++;  //need to do something with this
-    // questionEl.textContent = "Correct! The answer is " + quizArray[questionIndex].answer;  THIS ISN'T WORKING 
+    // questionEl.textContent = "Correct! The answer is " + quizArray[questionIndex].answer;  THIS ISN'T WORKING is it because of where it is in the function?  No time? no it doesn't work because our answer is now buttons.  ok. oh well. i tried. 
   }
   else {
-    timerCount = timerCount - timerPenalty;  //takes 5 seconds off for wrong answer
-    // questionEl.textContent = "Wrong! The answer is " + quizArray[questionIndex].answer; THIS ISN'T WORKING
+    timerCount = timerCount - timerPenalty;  //takes 10 seconds off for wrong answer
+    // questionEl.textContent = "Wrong! The answer is " + quizArray[questionIndex].answer; THIS ISN'T WORKING is because same as above. 
   }
   questionIndex++;
   if (questionIndex < quizArray.length) {
@@ -93,22 +99,21 @@ function checkAnswer(index) {
 
 // The following function renders questions/options as buttons
 function renderQuestions() {
-    let currentQuestion = quizArray[questionIndex];
-    questionText.textContent = currentQuestion.question;
+  let currentQuestion = quizArray[questionIndex];
+  questionText.textContent = currentQuestion.question;
   for (var i = 0; i < currentQuestion.options.length; i++) {
-        var choice = currentQuestion.options[i];
-        let buttonEl = document.getElementById("button" + i); 
-        buttonEl.textContent = choice;
+    var choice = currentQuestion.options[i];
+    let buttonEl = document.getElementById("button" + i);
+    buttonEl.textContent = choice;
   }
 }
 
 
 // function endQuiz() {
 //   questionArea.classList.add("hide");  //not sure if needed
-//   scoreEl.textContent = score;
-
-
-
+//   scoreEl.textContent = 'You scored " + score + "out of 5 correctly!";
+//  submit.classList.remove("hide");
+//  highScore();
 // }
 
 //still need a reset high score button
